a58e8295ee112c78ed51ff83516e35d82fec5019
Jason <jasondoroughonfb@live.com>
2025-05-03 03:46:32 -0700

    Fourth commit

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // http://www.omdbapi.com/?apikey=[yourkey]&
 // aac2feb4
+// http://www.omdbapi.com/?apikey=[aac2feb4]&
 
 
+
+async function main() {
+    const movies = await fetch(`http://www.omdbapi.com/?apikey=[aac2feb4]&`);
+    const movieData = await users.json();
+    console.log(movieData);
+
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join(``);
+
+}
