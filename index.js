48458c22782dc51ac91da6ee262aa47d95c0d8fc
Jason <jasondoroughonfb@live.com>
2025-05-03 23:22:19 -0700

    Fifth commit

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ async function main() {
     userListEl.innerHTML = usersData.map((user) => userHTML(user)).join(``);
 
 }
+
+
+// http://www.omdbapi.com/?apikey=[yourkey]&
+// aac2feb4
+// http://www.omdbapi.com/?apikey=[aac2feb4]&
+
+
+
+async function main() {
+    const movies = await fetch(`http://www.omdbapi.com/?apikey=[aac2feb4]&`);
+    const movieData = await users.json();
+    console.log(movieData);
+
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join(``);
+
+}
