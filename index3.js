3df8de9bfade633c171f3722b6e813f9b8996660
Jason <jasondoroughonfb@live.com>
2025-05-04 23:05:50 -0700

    Sixth commit

--- index3.js
+++ index3.js
@@ -29,3 +29,19 @@ function userHTML(user) {
                 </div>
             </div>`;
 }
+
+function showUserPosts(id) {
+    localStorage.setItem("id", id);
+    window.location.href = `${window.location.origin}/user.html`
+}
+
+function userHTML(user) {
+    return `<div class="user-card" onclick="showUserPosts(${user.id})">
+                <div class="user-card__container">
+                    <h3>${user.name}</h4>
+                    <p><b>Email:</b> ${user.email}</p>
+                    <p><b>Phone:</b> ${user.phone}</p>
+                    <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+                </div>
+            </div>`;
+}
